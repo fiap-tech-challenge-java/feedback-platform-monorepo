5fcdbaee24f026fbc45b2fbf6fd134599228a8cb
TeT95 <lucasfariascontato@gmail.com>
2026-01-21 23:40:44 -0300

    refactor: pequenos ajustes

--- feedback-core/src/main/java/br/com/postech/feedback/core/config/AwsConfigConstants.java
+++ feedback-core/src/main/java/br/com/postech/feedback/core/config/AwsConfigConstants.java
@@ -2,10 +2,13 @@ package br.com.postech.feedback.core.config;
 
 import lombok.experimental.UtilityClass;
 
-@UtilityClass // Garante que é uma classe estática utilitária
+/**
+ * Constantes para recursos AWS compartilhados entre os serviços.
+ */
+@UtilityClass
 public class AwsConfigConstants {
 
-    // Nomes exatos definidos no script init-aws.sh
+    // Nomes dos recursos AWS
     public static final String QUEUE_INGESTION_ANALYSIS = "feedback-analysis-queue";
     public static final String TOPIC_NOTIFICATION = "feedback-notification-topic";
     public static final String BUCKET_REPORTS = "postech-feedback-reports";
