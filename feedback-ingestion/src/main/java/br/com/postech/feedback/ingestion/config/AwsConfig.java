24be98feca9f264e19b82704624eada6e33c2aa8
TeT95 <lucasfariascontato@gmail.com>
2026-01-22 21:46:43 -0300

    fix: Corrige credentials

--- feedback-ingestion/src/main/java/br/com/postech/feedback/ingestion/config/AwsConfig.java
+++ feedback-ingestion/src/main/java/br/com/postech/feedback/ingestion/config/AwsConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.sqs.SqsClient;
@@ -13,24 +15,40 @@ import java.net.URI;
 @Configuration
 public class AwsConfig {
 
-    @Value("${spring.cloud.aws.region.static:us-east-1}")
+    @Value("${aws.region:us-east-1}")
     private String region;
 
-    @Value("${spring.cloud.aws.endpoint:}")
-    private String endpointUrl;
+    @Value("${aws.endpoint:#{null}}")
+    private String endpoint;
+
+    @Value("${aws.access-key:#{null}}")
+    private String accessKey;
+
+    @Value("${aws.secret-key:#{null}}")
+    private String secretKey;
 
     @Bean
     public SqsClient sqsClient() {
         var builder = SqsClient.builder()
-                .region(Region.of(region));
+                .region(Region.of(region))
+                .credentialsProvider(credentialsProvider());
 
-        if (endpointUrl != null && !endpointUrl.isBlank()) {
-            builder.endpointOverride(URI.create(endpointUrl))
-                    .credentialsProvider(StaticCredentialsProvider.create(
-                            AwsBasicCredentials.create("test", "test")));
+        if (isLocalStack()) {
+            builder.endpointOverride(URI.create(endpoint));
         }
 
         return builder.build();
     }
-}
 
+    private AwsCredentialsProvider credentialsProvider() {
+        if (isLocalStack()) {
+            return StaticCredentialsProvider.create(
+                    AwsBasicCredentials.create(accessKey, secretKey));
+        }
+        return DefaultCredentialsProvider.create();
+    }
+
+    private boolean isLocalStack() {
+        return endpoint != null && !endpoint.isEmpty();
+    }
+}
