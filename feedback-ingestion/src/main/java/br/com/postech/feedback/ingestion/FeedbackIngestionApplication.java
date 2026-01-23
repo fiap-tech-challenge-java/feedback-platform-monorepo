6119ac2799119ec89062e195fe7e786f6474709d
Larissa Damasceno de Lucena <larihlucena13@gmail.com>
2026-01-22 22:07:33 -0300

    add scan pacotes

--- feedback-ingestion/src/main/java/br/com/postech/feedback/ingestion/FeedbackIngestionApplication.java
+++ feedback-ingestion/src/main/java/br/com/postech/feedback/ingestion/FeedbackIngestionApplication.java
@@ -2,8 +2,12 @@ package br.com.postech.feedback.ingestion;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = "br.com.postech.feedback")
+@EnableJpaRepositories(basePackages = "br.com.postech.feedback.core.repository")
+@EntityScan(basePackages = "br.com.postech.feedback.core.domain")
 public class FeedbackIngestionApplication {
 
 	public static void main(String[] args) {
